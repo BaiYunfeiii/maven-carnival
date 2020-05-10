e4a5c36f291c7fc98bb1702086427d87635b3504
Yunfei Bai <baiyunfeiii@foxmail.com>
2020-05-10 11:10:42 +0800

    Automatically add git pre-push hook when running mvn generate-source phase

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,13 @@
-echo "Hello World!"
+#!/bin/sh
+
+echo "Configuring git pre-push hook"
+
+cp scripts/pre-commit.sh .git/hooks/pre-commit.sh
+
+SUCCESS=$0
+
+if [ $SUCCESS -gt 1 ]; then
+    echo "Configuration Failed"
+    exit $SUCCESS
+fi
+echo "Configuration Completed"
